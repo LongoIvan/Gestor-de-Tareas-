2e544ed265ecf386dd951fc495077c445bd924c7
IvanLongo <longo.ivan.91@gmail.com>
2026-09-17 19:58:31 -0300

    asdasdada2 git push sda

--- Main.py
+++ Main.py
@@ -4,5 +4,4 @@ listarTareas = [Tareas("Tarea 1"), Tareas("Tarea 2"), Tareas("Tarea 3")]
 Tareas.listar(listarTareas)
 listarTareas[0].completar()
 Tareas.listar(listarTareas)
-print(listarTareas[0].estado)
-print("hola mariano")
+print ("AJA")

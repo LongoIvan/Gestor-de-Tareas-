15d1e33c3987b5f355348782022b0d92fad05c06
Ivan Longo <longo.ivan.91@gmail.com>
2026-09-17 23:22:29 -0300

    asdasda

--- Main.py
+++ Main.py
@@ -4,12 +4,4 @@ listarTareas = [Tareas("Tarea 1"), Tareas("Tarea 2"), Tareas("Tarea 3")]
 Tareas.listar(listarTareas)
 listarTareas[0].completar()
 Tareas.listar(listarTareas)
-<<<<<<< HEAD
-print ("AJA")
-=======
-print(listarTareas[0].estado)
-print("hola mariano!adasdsad")
->>>>>>> 8ce53747d8df3ac6ab883a5afc855e68b50b27a5
-
-
-print("hola mariano!adasdsad")
+print("ola mundo")

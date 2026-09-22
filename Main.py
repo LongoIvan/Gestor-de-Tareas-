8b00d24a6211f07ff0e85fca9107a3f565c7bb2b
mariancampe13 <mariancampe13@gmail.com>
2026-09-22 17:32:23 -0300

    Prueba 2

--- Main.py
+++ Main.py
@@ -4,4 +4,4 @@ listarTareas = [Tareas("Tarea 1"), Tareas("Tarea 2"), Tareas("Tarea 3")]
 Tareas.listar(listarTareas)
 listarTareas[0].completar()
 Tareas.listar(listarTareas)
-print ("hola Ivan")
+print ("hola Soy Mariano")
